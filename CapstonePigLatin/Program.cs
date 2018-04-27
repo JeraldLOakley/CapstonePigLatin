7bf4e2ceb102124d2c3fa420a6b6e17af5bd9720
Jerald <Jerryoakley989@gmail.com>
2018-04-27 17:14:13 -0400

    first commit

---
+++ CapstonePigLatin/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstonePigLatin
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello and welcome to the Pig Latin anslatortray");
+			Console.WriteLine("Please type the word that you would like to translate");
+			string vowels = ("aeiou");
+			string word = Console.ReadLine().ToLower();
+			string firstLetter = word.Substring(0, 1);
+			if (firstLetter == "a" || firstLetter == "e" || firstLetter == "i" || firstLetter == "o" || firstLetter == "u")
+			{
+				Console.WriteLine(word + "way");
+			}
+			else
+				Consonent();
+		
+
+			Console.ReadKey();
+		}
+
+
+		static string Consonent(string word)
+		{
+			string vowels = ("aeiou");
+			
+			string firstLetter = word.Substring(0, 1);
+			for (int i = 0; i < word.Length; i++)
+			{
+				for (int j = 0; j < vowels.Length; j++)
+				{
+					if (word[i] == vowels[j])
+					{
+						int vowelPosition = i;
+						string consonent = word.Substring(0, vowelPosition);
+						string end = word.Substring(vowelPosition, word.Length - vowelPosition - 1);
+
+						return end + consonent + "ay";
+						
+					}
+					else return "no vowels";
+					
+
+				}
+			}
+
+		}
+
+
+	}
+}
